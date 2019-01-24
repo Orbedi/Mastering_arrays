9e21f6ce8bb4ab364eec59bc2635682c08f53c04
orbedi <orlandobedi@outlook.es>
2019-01-24 14:29:47 +0000

    Creation of the text_methods.py file and, within it, class Text and its functions.

---
+++ text_methods.py
@@ -0,0 +1,29 @@
+class Text():
+
+    text = ''
+
+    # Count the number of 'c' within the text.
+    def countChars(self, char):
+        return
+
+
+    # Gives the position within the text in which the EXACT “occurrence” (minding cases) has been firstly found.
+    def indexOf(self, occurrence):
+        return
+
+    #  A word or group of words that is the same when you read it forwards from the beginning or backwards from the end
+    def isPalindrome(self):
+        return
+
+    # Counts the numbers of "occurrence" within text. Search must be case insensitive.
+    def countOccurrence(self, occurrence):
+        return
+
+    # Counts the words that have a length grater than wordLength
+    def countWords(self, wordLendth):
+        return
+
+    # Constructor
+    def __init__(self, characters):
+        global text
+        text = characters
